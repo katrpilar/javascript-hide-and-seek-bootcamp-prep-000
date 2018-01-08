752286efbdd39e88ed6c676f2a5ab4b64dca86dc
Katrina Brinson <kbrinso@gmail.com>
2018-01-08 00:57:25 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < lis.length; i++){
+    var tonum = parseInt(lis[i].innerHTML);
+    var val = tonum + n;
+    lis[i].innerHTML = val.toString();
+  }
+}
+
+function deepestChild(){
+    const lis = document.querySelector('div#grand-node').querySelectorAll('div');
+    const last = lis.length - 1;
+    return (lis[last]);
+}
